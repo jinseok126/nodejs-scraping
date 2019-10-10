7676b51c435304cc8dba4dd29f8206242ed30a83
jinseok126 <jinseok126@naver.com>
2019-10-10 14:46:16 +0900

    parsing data save

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ router.get("/test", function(req, res){
 router.get("/", function(req, res){
 
     const URL = "https://www.concepts1one.co.kr";
-    const SEARCH = "슬랙스";
-
+    const SEARCH = "티셔츠";
+    
     (async () => {
         // 검색어 count
         let searchCount = await models.Searchs.count({
@@ -25,7 +25,10 @@ router.get("/", function(req, res){
         }).then((results)=> {
            return results;
         });
-        const productArr = new Array();
+
+        let productArr = new Array();
+        const x = new Array();
+        const x1 = new Array();
 
         // 존재하지 않는 검색어일 경우 데이터를 확인하고 데이터가 존재할 경우 검색어 저장 AND 검색 결과 데이터도 저장
         if(searchCount === 0) {
@@ -36,13 +39,6 @@ router.get("/", function(req, res){
                     search_str: SEARCH
                 }
             }
-            const product = {
-                searchName: SEARCH,
-                productName: "",
-                salePrice: 0,
-                costPrice: 0
-            }
-            
             
             // 몇개의 페이지가 구성되어있는지 확인하는 request
             await request(option, function(error, response, body){
@@ -52,8 +48,6 @@ router.get("/", function(req, res){
                 let $ = cheerio.load(body);
                 let divSearch = $("div.prd_search");
                 const checkData = divSearch.find(".empty").text();
-
-                console.log(checkData);
 
                 // 데이터가 존재할 경우
                 if(checkData === ""){
@@ -69,6 +63,12 @@ router.get("/", function(req, res){
 
             // 데이터가 존재하는 경우
             if(firstNum !== 0 && secondNum !== 0) {
+
+                // search insert 후 index 추출
+                const searchIdxData = await models.Searchs.create({searchName:SEARCH}).then( (result) => {
+                    return result.dataValues.idx;
+                })
+                
                 // 데이터 파싱 부분
                 for(let i=1; i<secondNum-firstNum+2; i++) {
                     const option = {
@@ -85,28 +85,48 @@ router.get("/", function(req, res){
                             search_str: SEARCH
                         }
                     } // option
-
+                    
                     await request(option, function(error, response, body) {
                     
                         const $ = cheerio.load(JSON.parse(body).content);
                         
-                        divSearch = $("li .box");
+                        const divSearch = $("li .box");
 
                         // 페이징 데이터
                         divSearch.find(".info").each(function(index, elem) {
-                            product.productName = $(this).find(".name a").text();
-                            product.salePrice = parseInt($(this).find(".price").find(".sell").text().split(",").join("").split(" ")[0]);
-                            product.costPrice = parseInt($(this).find(".price").find(".consumer").text().split(",").join("").split(" ")[0]);
-                            productArr.push(product);
+
+                            const product = {
+                                searchIdx: searchIdxData,
+                                productName: $(this).find(".name a").text(),
+                                salePrice: parseInt($(this).find(".price").find(".sell").text().split(",").join("").split(" ")[0]),
+                                costPrice: parseInt($(this).find(".price").find(".consumer").text().split(",").join("").split(" ")[0])
+                            }
+                            x.push(product);
                         });
                     }); // request
+
+                    productArr = productArr.concat(x);
                 } // end for
 
+                
+                // 중복 데이터 제거 부분
+                const refreshData = productArr.filter((item, i) => {
+                    return productArr.findIndex((item2, j) => {
+                        return item.productName === item2.productName;
+                    }) === i;
+                });
+                
+                // console.log(refreshData);
+                await models.Product.bulkCreate(refreshData);
             } // end if
         } // end if
+
+        
+
         res.send(productArr);
         res.end();
     })();
+
 }); // router
 
 module.exports = router;
