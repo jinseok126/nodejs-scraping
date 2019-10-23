65e9f2a8c729e07d0006aac7d68113e29ac4fdfc
jinseok126 <jinseok126@naver.com>
2019-10-23 17:17:41 +0900

    nodejs-jwt add vue router Access Per Token

--- nodejs-jwt/frontend/src/routes/router.js
+++ nodejs-jwt/frontend/src/routes/router.js
@@ -4,8 +4,30 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Join from '../views/Join.vue'
 import AdminHome from '../views/admin/AdminHome'
+import store from '../store'
+import jwt_decode from 'jwt-decode'
 
 Vue.use(Router)
+
+const adminCheck = () => (to, from, next) => {
+  const token = store.getters.getToken;
+
+  // 토큰이 없을 경우
+  if(token === null || token === "") {
+    alert("로그인 후 다시 시도해 주시기 바랍니다.");
+    next("/");
+
+  // 토큰이 존재할 경우 
+  } else {
+    const decoded = jwt_decode(token);
+    if(decoded.roleName !== "admin") {
+      alert(`${decoded.roleName}는 접근할 수 없습니다.`);
+      next("/");
+    }
+  }
+  
+  return next();   // 인증 성공 시
+};
 
 export default new Router({
   mode: 'history',
@@ -14,7 +36,7 @@ export default new Router({
     { path: '/', name: 'home', component: Home },
     { path: '/login', name: 'login', component: Login },
     { path: '/join', name: 'join', component: Join },
-    { path: '/admin', name: 'adminHome', component: AdminHome }
+    { path: '/admin', name: 'adminHome', component: AdminHome, beforeEnter: adminCheck() }
   ]
 })
 
--- nodejs-jwt/frontend/src/views/admin/AdminHome.vue
+++ nodejs-jwt/frontend/src/views/admin/AdminHome.vue
@@ -22,6 +22,6 @@ export default {
         console.log(result);
       });
     }
-  }
+  } // methods
 }
 </script>
