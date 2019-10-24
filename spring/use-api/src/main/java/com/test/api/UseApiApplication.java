5b6f54cc8bdc254771a2afebe38c7c2e600eb0fc
jinseok126 <jinseok126@naver.com>
2019-10-24 14:45:51 +0900

    use-api spring boot setting

---
+++ spring/use-api/src/main/java/com/test/api/UseApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseApiApplication.class, args);
+	}
+
+}
