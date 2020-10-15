05d967f2f6792ee1367fd0cb18a93d968b2399aa
jinseok126 <jinseok126@naver.com>
2020-10-16 00:48:32 +0900

    vue router spring boot connect

--- spring/chat/src/main/java/com/kjs/demo/controller/DemoController.java
+++ spring/chat/src/main/java/com/kjs/demo/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/demo")
 	public String demo() {
 		return "Demo";
 	}
--- spring/chat/src/main/java/com/kjs/demo/controller/ViewController.java
+++ spring/chat/src/main/java/com/kjs/demo/controller/ViewController.java
@@ -1,4 +1,13 @@
 package com.kjs.demo.controller;
 
+import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ViewController {
+
+    @RequestMapping(value = {"/", "/about"})
+    public String view() {
+        return "forward:/index.html";
+    }
 }
