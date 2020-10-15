5734a8d08be2a0f7e98e1d7c7c6a266616d72fd0
jinseok126 <jinseok126@naver.com>
2020-10-15 22:08:16 +0900

    spring boot init

---
+++ spring/chat/src/main/java/com/kjs/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.kjs.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@RequestMapping("/")
+	public String demo() {
+		return "Demo";
+	}
+
+}
